483ce666a94a1887372cfd7caf892b201c6e9d5e
Luis Johann <luisfj_pr@hotmail.com>
2024-11-05 18:21:48 -0300

    Changes for prod debug

--- src/components/nextAuthProvider.tsx
+++ src/components/nextAuthProvider.tsx
@@ -1,11 +1,22 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
+import { getSession, SessionProvider } from "next-auth/react";
 
 type Props = {
   children?: React.ReactNode;
+  session: Session | null;
 };
 
-export const NextAuthProvider = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
+export const NextAuthProvider = ({ children, session }: Props) => {
+  return <SessionProvider baseUrl={process.env.NEXTAUTH_URL} refetchInterval={60 * 5} session={session}>{children}</SessionProvider>;
 };
+
+// NextAuthProvider.getInitialProps = async (context) => {
+//   const { ctx } = context;
+//   const session = await getSession(ctx);
+
+//   return {
+//     session,
+//   };
+// };
