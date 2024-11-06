a94e1b183013a4658834a9e30b8815c47cb79bca
Luis Johann <luisfj_pr@hotmail.com>
2024-11-06 14:00:12 -0300

    include envs

--- src/types/node-env.d.ts
+++ src/types/node-env.d.ts
@@ -4,5 +4,7 @@ declare namespace NodeJS {
       KEYCLOAK_CLIENT_SECRET: string
       KEYCLOAK_ISSUER: string
       AUTH_SECRET: string
+      BASE_BACKEND_URL: string
+      KEYCLOAK_BASE_URL: string
     }
   }
