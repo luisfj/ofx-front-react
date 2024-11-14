d57326b10268e38eee68d91655534c88ad5bb41f
Luis Johann <luisfj_pr@hotmail.com>
2024-11-14 08:54:16 -0300

    Ajusta arquivos publicos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,6 @@ export default function RootLayout({
 
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" />
 
-
           <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
 
         </head>
